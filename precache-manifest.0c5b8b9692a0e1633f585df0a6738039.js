1de82080fdfedec34f4cbd7f09ffafdc09794d3d
Rene Laverdure <renelaverdure@gmail.com>
2020-11-12 02:30:37 +1100

    Updates

---
+++ precache-manifest.0c5b8b9692a0e1633f585df0a6738039.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49210b7a1174c279d4282fcf95b001cb",
+    "url": "/ButtercupBakery/index.html"
+  },
+  {
+    "revision": "1951da5349f72cb5eb86",
+    "url": "/ButtercupBakery/static/css/main.abb21d9f.chunk.css"
+  },
+  {
+    "revision": "ac936aff31e3117fa16a",
+    "url": "/ButtercupBakery/static/js/2.135941f8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ButtercupBakery/static/js/2.135941f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1951da5349f72cb5eb86",
+    "url": "/ButtercupBakery/static/js/main.88a21795.chunk.js"
+  },
+  {
+    "revision": "1dc223ec843ccf77734c",
+    "url": "/ButtercupBakery/static/js/runtime-main.f8ed2748.js"
+  },
+  {
+    "revision": "90c5919f6a46a5764f64974d863323cd",
+    "url": "/ButtercupBakery/static/media/4-tier-cake.90c5919f.jpg"
+  },
+  {
+    "revision": "661a0bbd5712de6db2352aa1f45183be",
+    "url": "/ButtercupBakery/static/media/Adidas_Logo.661a0bbd.png"
+  },
+  {
+    "revision": "e8f9266e3681b9de5eb6221c53fe44cf",
+    "url": "/ButtercupBakery/static/media/Chocolate-Macarons.e8f9266e.jpg"
+  },
+  {
+    "revision": "88b14d0967c9065a6b2969b114dcb1ce",
+    "url": "/ButtercupBakery/static/media/Chocolate-cupcake-strawberry-topping.88b14d09.jpg"
+  },
+  {
+    "revision": "102c0b24c57ac1c88e21084d1da0fb51",
+    "url": "/ButtercupBakery/static/media/Danish-butter-cake.102c0b24.jpg"
+  },
+  {
+    "revision": "563d9d882115a35a744fd16832337dff",
+    "url": "/ButtercupBakery/static/media/Fudge-Brownies-3.563d9d88.jpg"
+  },
+  {
+    "revision": "115253eb01e78d5f7e7bb551e44a6df5",
+    "url": "/ButtercupBakery/static/media/Lemon-Macarons.115253eb.jpg"
+  },
+  {
+    "revision": "1651ec8529c0c569c219549a77c34dfd",
+    "url": "/ButtercupBakery/static/media/Macaron-strawberry.1651ec85.jpg"
+  },
+  {
+    "revision": "f7d2e2d2f5c71c313f14d237910bf40b",
+    "url": "/ButtercupBakery/static/media/Macarons-blueberry.f7d2e2d2.jpg"
+  },
+  {
+    "revision": "4c36a5172e803651fdc0d5c94a38ba52",
+    "url": "/ButtercupBakery/static/media/Nutella-Donut.4c36a517.jpg"
+  },
+  {
+    "revision": "0790de2f606d12ddd789870cc87e4c92",
+    "url": "/ButtercupBakery/static/media/Sponge-Cake-strawberry.0790de2f.jpg"
+  },
+  {
+    "revision": "9a1a58e48d1bd99e68b7a59dce8d1831",
+    "url": "/ButtercupBakery/static/media/Westfield-logo.9a1a58e4.png"
+  },
+  {
+    "revision": "aa0def7fa0ed4ba2ff2bf43b7cf11601",
+    "url": "/ButtercupBakery/static/media/activision-logo.aa0def7f.jpg"
+  },
+  {
+    "revision": "56327c324698f7d6d7bcaef969d811b7",
+    "url": "/ButtercupBakery/static/media/bakery-ingret.56327c32.jpg"
+  },
+  {
+    "revision": "903132f0be2b12518a9e66355d58ef23",
+    "url": "/ButtercupBakery/static/media/bakery.903132f0.svg"
+  },
+  {
+    "revision": "128073a469de079390e7b517f90d3e84",
+    "url": "/ButtercupBakery/static/media/blueberry-.128073a4.jpg"
+  },
+  {
+    "revision": "d2a6cbff5855731ec64fad372230c667",
+    "url": "/ButtercupBakery/static/media/blueberry-donuts.d2a6cbff.jpg"
+  },
+  {
+    "revision": "2b1b53af2cff4ce69741ff4a722b46d1",
+    "url": "/ButtercupBakery/static/media/cake-1.2b1b53af.svg"
+  },
+  {
+    "revision": "8275b91ac447e6424d40d2d958c9ea87",
+    "url": "/ButtercupBakery/static/media/cake.8275b91a.svg"
+  },
+  {
+    "revision": "5596b2b6b7110420c62be9992e5ccfa5",
+    "url": "/ButtercupBakery/static/media/cakes-display.5596b2b6.jpg"
+  },
+  {
+    "revision": "fc1398b08c211b554277dbc4ba9a3439",
+    "url": "/ButtercupBakery/static/media/cakespread.fc1398b0.png"
+  },
+  {
+    "revision": "a97717b920c83d72d307cea235357792",
+    "url": "/ButtercupBakery/static/media/chocolate-berry-sponge.a97717b9.jpg"
+  },
+  {
+    "revision": "1a62c8355bf35aca379a5cad3dfb8646",
+    "url": "/ButtercupBakery/static/media/chocolate-cupcake.1a62c835.jpg"
+  },
+  {
+    "revision": "74991494a037aa8f3c2ab315e0e280b1",
+    "url": "/ButtercupBakery/static/media/chocolate-cupcakes.74991494.jpg"
+  },
+  {
+    "revision": "c3adc49517df0479ab584cb589d27fcf",
+    "url": "/ButtercupBakery/static/media/chocolate-mud.c3adc495.jpg"
+  },
+  {
+    "revision": "af6b2e09978e11d050158fd6470bd160",
+    "url": "/ButtercupBakery/static/media/chocolate-shaving-cupcake.af6b2e09.jpg"
+  },
+  {
+    "revision": "040e31f72a56e5aebe6715429b5ca84c",
+    "url": "/ButtercupBakery/static/media/chocolate-sponge-cake.040e31f7.jpg"
+  },
+  {
+    "revision": "5912cd129db69f34aaa33ee67c495798",
+    "url": "/ButtercupBakery/static/media/chocolate-top-cakes.5912cd12.jpg"
+  },
+  {
+    "revision": "3e9953214d660702c39d92359e12370b",
+    "url": "/ButtercupBakery/static/media/chocolate-top-donut.3e995321.jpg"
+  },
+  {
+    "revision": "a9105d062de729b18b35b06027057b66",
+    "url": "/ButtercupBakery/static/media/cocholate-cake-pink.a9105d06.png"
+  },
+  {
+    "revision": "d2c4341ceb557f3359d1aca856e98cce",
+    "url": "/ButtercupBakery/static/media/cup-cake.d2c4341c.svg"
+  },
+  {
+    "revision": "dfe5af249a7d331fa294197dafcf5005",
+    "url": "/ButtercupBakery/static/media/deakin-logo.dfe5af24.png"
+  },
+  {
+    "revision": "3f7446b6b20651d1d8de524dacd934be",
+    "url": "/ButtercupBakery/static/media/donuts-display.3f7446b6.jpg"
+  },
+  {
+    "revision": "e6a2135c2980a69537c55fcbf2d1e87d",
+    "url": "/ButtercupBakery/static/media/donuts-lemon.e6a2135c.jpg"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/ButtercupBakery/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "5e7565c5ed014a62b2a346b3c8b5ff9a",
+    "url": "/ButtercupBakery/static/media/french-macaron-mix.5e7565c5.jpg"
+  },
+  {
+    "revision": "418344efde24bf50561c71e44b1697f2",
+    "url": "/ButtercupBakery/static/media/french-macaron.418344ef.jpg"
+  },
+  {
+    "revision": "7594cbea5d4e13d94c5a1cf149bf6fe2",
+    "url": "/ButtercupBakery/static/media/fudgy-brownies.7594cbea.jpg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/ButtercupBakery/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "a450619affa0ef6c40e80bf39e4cba47",
+    "url": "/ButtercupBakery/static/media/lemon-sponge.a450619a.jpg"
+  },
+  {
+    "revision": "85fcc8021d206181b388d8e8b4510a0d",
+    "url": "/ButtercupBakery/static/media/macarons.85fcc802.svg"
+  },
+  {
+    "revision": "7a3389bfe4b48527a53d094097a18d88",
+    "url": "/ButtercupBakery/static/media/macroos.7a3389bf.jpg"
+  },
+  {
+    "revision": "b430b0ca670ce04330f341f9a2e824e5",
+    "url": "/ButtercupBakery/static/media/nike-logo.b430b0ca.png"
+  },
+  {
+    "revision": "4d8f3e6f6635230dedd0009d7a058051",
+    "url": "/ButtercupBakery/static/media/oven.4d8f3e6f.svg"
+  },
+  {
+    "revision": "10185870b29419ce6e1eb96eff0c97af",
+    "url": "/ButtercupBakery/static/media/pancake-tower.10185870.jpg"
+  },
+  {
+    "revision": "5a4db344106d8c79460e693b6ed5d1b3",
+    "url": "/ButtercupBakery/static/media/pinterest.5a4db344.svg"
+  },
+  {
+    "revision": "7f68455073833ce83f961c4c944ff6ac",
+    "url": "/ButtercupBakery/static/media/rainbow-cake.7f684550.jpg"
+  },
+  {
+    "revision": "31a82b13902c3499041046a06a8849e5",
+    "url": "/ButtercupBakery/static/media/rainbow-stack.31a82b13.jpeg"
+  },
+  {
+    "revision": "f300100373907a5e5f3c6b8c366035be",
+    "url": "/ButtercupBakery/static/media/shipping.f3001003.svg"
+  },
+  {
+    "revision": "fc24d304a52e70968b845dffb164fe65",
+    "url": "/ButtercupBakery/static/media/shopping-cart.fc24d304.svg"
+  },
+  {
+    "revision": "106fc798ff14d27eef9aba70aa838b07",
+    "url": "/ButtercupBakery/static/media/short-cakes.106fc798.jpg"
+  },
+  {
+    "revision": "7c9de3ebd5eedf5abc29b45cae9120b0",
+    "url": "/ButtercupBakery/static/media/sponge-cake.7c9de3eb.jpg"
+  },
+  {
+    "revision": "6ee55695e21091a061d185eafdaf8847",
+    "url": "/ButtercupBakery/static/media/stacked-donuts.6ee55695.jpg"
+  },
+  {
+    "revision": "ddfb7afaa6dc02e6e6691e3566844fe2",
+    "url": "/ButtercupBakery/static/media/store.ddfb7afa.svg"
+  },
+  {
+    "revision": "0ae2127b34eb1af73506ef5ad87da05f",
+    "url": "/ButtercupBakery/static/media/strawberry-cupcake.0ae2127b.jpg"
+  },
+  {
+    "revision": "96f17059a268a922102498f0dc0bda95",
+    "url": "/ButtercupBakery/static/media/strawberry-top-scones.96f17059.jpg"
+  },
+  {
+    "revision": "0526f0e23ef15514679f4ae8e5edae1e",
+    "url": "/ButtercupBakery/static/media/sweet-macarons.0526f0e2.jpg"
+  },
+  {
+    "revision": "63bc75cdf8c999b8962fac2858e49add",
+    "url": "/ButtercupBakery/static/media/triple-chocolate-cupcakes.63bc75cd.jpg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/ButtercupBakery/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "0328a85828d12ce86e8db2d935aeb834",
+    "url": "/ButtercupBakery/static/media/vanilla-butter-cake.0328a858.jpg"
+  },
+  {
+    "revision": "0014ac8c7d2ff36aad8d3ddca93e63fd",
+    "url": "/ButtercupBakery/static/media/vanilla-cupcakes-chocolate-frosting.0014ac8c.jpg"
+  },
+  {
+    "revision": "8da9b211ac17eb3cc8b137dd877f72e0",
+    "url": "/ButtercupBakery/static/media/vanilla-cupcakes.8da9b211.jpg"
+  },
+  {
+    "revision": "b74ae0a5bed8502279a556219b2aae8d",
+    "url": "/ButtercupBakery/static/media/victorian-sponge-cake-strawberry.b74ae0a5.jpg"
+  }
+]);
